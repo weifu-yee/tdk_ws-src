2c31359ce67ca4606ebe6841f2cd39515e834ed3
weifu-yee <chenweifu9999@gmail.com>
2023-10-07 16:08:00 +0000

    yaml_path.h push一下

---
+++ main_func/include/main_func/yaml_path.h
@@ -0,0 +1,13 @@
+#ifndef _YAML_PATH_H_
+#define _YAML_PATH_H_
+
+// #define buildNodeFilePath "/home/ditrobotics/tdk_ws/src/main_func/params/buildNode.yaml"
+// #define initBuildEdgeFilePath "/home/ditrobotics/tdk_ws/src/main_func/params/initBuildEdge.yaml"
+#define buildNodeFilePath "/root/tdk_ws/src/main_func/params/buildNode.yaml"
+#define initBuildEdgeFilePath "/root/tdk_ws/src/main_func/params/initBuildEdge.yaml"
+
+
+// #define pub_numPath "/home/ditrobotics/tdk_ws/src/main_func/params/pub_num.yaml"
+#define pub_numPath "/root/tdk_ws/src/main_func/params/pub_num.yaml"
+
+#endif
