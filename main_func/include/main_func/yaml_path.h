cd0d8ea85296cb04f0e5b16ad82dea0bf3f587be
weifu-yee <chenweifu9999@gmail.com>
2023-10-08 18:04:07 +0800

    master on Rasp 2

--- main_func/include/main_func/yaml_path.h
+++ main_func/include/main_func/yaml_path.h
@@ -3,11 +3,10 @@
 
 #define buildNodeFilePath "/home/ditrobotics/tdk_ws/src/main_func/params/buildNode.yaml"
 #define initBuildEdgeFilePath "/home/ditrobotics/tdk_ws/src/main_func/params/initBuildEdge.yaml"
+#define pub_numPath "/home/ditrobotics/tdk_ws/src/main_func/params/pub_num.yaml"
+
+// #define pub_numPath "/root/tdk_ws/src/main_func/params/pub_num.yaml"
 // #define buildNodeFilePath "/root/tdk_ws/src/main_func/params/buildNode.yaml"
 // #define initBuildEdgeFilePath "/root/tdk_ws/src/main_func/params/initBuildEdge.yaml"
-
-
-#define pub_numPath "/home/ditrobotics/tdk_ws/src/main_func/params/pub_num.yaml"
-// #define pub_numPath "/root/tdk_ws/src/main_func/params/pub_num.yaml"
 
 #endif
