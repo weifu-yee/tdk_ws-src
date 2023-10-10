384737b58adbd6c0e5d5890221bf08b95abdcd9b
weifu-yee <chenweifu9999@gmail.com>
2023-10-10 04:09:36 +0000

    CAM::predict_numbers

--- main_func/include/main_func/cammera.h
+++ main_func/include/main_func/cammera.h
@@ -7,6 +7,8 @@ namespace CAM{
     extern std::set<int> numbers;
     extern bool cease;
     extern double capt_x[];
+    extern std::vector<std::vector<int>> predict_numbers;
+    void initPredictNumbers(void);
     void what_to_erase(int a, int b);
 }
 using namespace CAM;
